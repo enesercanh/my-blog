82d76e6133fe7d21f0eb7fd59fb6f27bb0bf455b
Enes Ercan Enwar Hurmuzlu <enesalhurmuzi@gmail.com>
2025-01-01 02:30:02 -0800

    Delete testEmail.js

--- testEmail.js
+++
@@ -1,5 +0,0 @@
-const sendEmail = require('./emailService');
-
-sendEmail('recipient-email@example.com', 'Test Subject', 'This is a test email!')
-  .then(() => console.log('Email sent successfully!'))
-  .catch((err) => console.error('Failed to send email:', err));
